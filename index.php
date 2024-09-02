3c20c3f4505596d850f9427ee316a94e952c3015
mystylestore <mystylestore06@gmail.com>
2024-09-02 12:56:34 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<script src="https://kit.fontawesome.com/84c66eb9f0.js" crossorigin="anonymous"></script>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<title>TERRAMINER</title>
+</head>
+<body>
+		<div class="menu-content" >  
+				<div class="titulo"> 
+					<div>
+						<video class="video"  src="video/terraminer.mp4" height="125px"  width="450px" autoplay muted loop ></video>
+					</div>
+				 </div>
+
+				<div class="menu">
+					<nav class="menu-cuerpo">
+						<a href=""><span class="material-symbols-outlined">mail_lock</span>Webmail</a>
+						<a href=""><span class="material-symbols-outlined">login</span>Empresa</a>		
+						<a href=""><span class="material-symbols-outlined">design_services</span>Servicios</a>
+						<a href=""><span class="material-symbols-outlined">home</span>Inicio</a>
+					</nav>
+				</div>
+		</div>
+
+			<header  class="contenido-body">
+
+				<div class="baner-1">
+					<div class="texto-baner" >	
+							<h1>terraminer</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+						<img src="img/baner.jpg" height="100%" width="100%" >
+				</div>
+
+			
+
+
+
+					<div class="banner-2" >
+							
+					<div class="contedio" > 
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+					<div class="contedio" > 
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+					<div class="contedio" > 
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+
+					</div>
+			</header>
+
+			<footer class="bajo">
+				<span class="material-symbols-outlined">copyright</span><div class="texto-footer" >Todos los derechos reservados</div>
+			</footer>
+</body>
+</html>
